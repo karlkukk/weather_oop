63161eae939fba351f562a07e18bcbb31cbfdea9
karlkukk <karlkukk97@gmail.com>
2020-12-17 14:51:24 +0200

    Change city

--- App.js
+++ App.js
@@ -2,6 +2,19 @@
 const weather = new Weather('Tartu linn');
 // app UI object
 const ui = new UI();
-weather.weatherData().then(cityWeather => {
-    ui.print(cityWeather);
-}).catch(error => console.log(error));
+
+const changeBtn = document.querySelector('#w-change');
+changeBtn.addEventListener('click', changeWeather);
+
+function changeWeather() {
+    const city = document.querySelector('#city').value;
+    weather.changeCityName(city);
+    drawWeather();
+    $('#changeCity').modal('hide');
+}
+
+function drawWeather() {
+    weather.weatherData().then(cityWeather => {
+        ui.print(cityWeather);
+    }).catch(error => console.log(error));
+}
