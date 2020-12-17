bf1fbc8dfb7b67831858d9eb725f3a35d8b816af
karlkukk <karlkukk97@gmail.com>
2020-12-17 14:41:27 +0200

    Humidity

--- UI.js
+++ UI.js
@@ -3,12 +3,14 @@ class UI {
         this.cityName = document.querySelector('#w-city');
         this.temp = document.querySelector('#w-temp');
         this.desc = document.querySelector('#w-desc');
+        this.hum = document.querySelector('#w-hum');
     }
 
     print(cityWeather) {
         this.cityName.textContent = cityWeather.name;
         const celcius = Math.round(parseFloat(cityWeather.main.temp) - 273.15);
-        this.temp.innerHTML = `${celcius} &deg;;C`;
+        this.temp.innerHTML = `${celcius} &deg;C`;
         this.desc.textContent = cityWeather.weather[0].description;
+        this.hum.innerHTML = `${cityWeather.main.humidity}%`;
     }
 }
